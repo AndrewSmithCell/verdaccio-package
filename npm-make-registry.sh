117070d8a88ceb23f439ae16d44b984fd425e195
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-11-10 08:44:40 +0900

    Create npm-make-registry.sh

---
+++ npm-make-registry.sh
@@ -0,0 +1,6 @@
+python3 npm-registry.py
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add .
+git commit -m "AUTO updated sequence number"
+git push --force
