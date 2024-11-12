6a552735113113976c9ccbbf653e4c692593b5e9
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-11-12 16:31:41 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -1,2 +1,2 @@
 # NPM_CONFIG_REGISTRY=http://localhost:4873 npm pack --dry-run sass postcss-cli postcss autoprefixer qu ws simplesets mathoid
-cp ../package-jsons/package.json package.json && NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
+cp -f ../package-jsons/package.json package.json && NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
