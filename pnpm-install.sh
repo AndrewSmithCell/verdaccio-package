9dbf91ee5f522f43971e288d04c1c13509338aae
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-12-24 16:16:55 +0900

    Update pnpm-install.sh

--- pnpm-install.sh
+++ pnpm-install.sh
@@ -1,5 +1,5 @@
 rm package.json
-rm package-lock.json
+rm pnpm-lock.yaml
 cp -f ../package-jsons/package.json package.json
 cp -f ../package-jsons/pnpm-lock.yaml pnpm-lock.yaml
 NPM_CONFIG_REGISTRY=http://localhost:4873 pnpm install
