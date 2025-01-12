2edd152794d4fb0b740210ccc41b3182016ec99d
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-01-12 11:02:30 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -1,5 +1,5 @@
 rm package.json
 rm package-lock.json
-cp -f ../package-jsons/package.json package.json
-cp -f ../package-jsons/package-lock.json package-lock.json
-NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
+# cp -f ../package-jsons/package.json package.json
+# cp -f ../package-jsons/package-lock.json package-lock.json
+NPM_CONFIG_REGISTRY=http://localhost:4873 pnpm create-next-app --ts --tailwind --eslint --turbopack --use-pnpm --yes
