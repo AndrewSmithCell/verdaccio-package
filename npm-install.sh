d626feb88e172fbc6bd995fa3069f6f0249d3fa0
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-01-17 10:20:41 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -2,4 +2,4 @@ rm package.json
 rm package-lock.json
 cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/package-lock.json package-lock.json
-NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
+NPM_CONFIG_REGISTRY=http://localhost:4873 npm install --force
