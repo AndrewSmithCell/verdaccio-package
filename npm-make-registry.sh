4135da51e92be2ced1fcc77ff9df89368817008a
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-02-13 11:44:01 +0900

    Update npm-make-registry.sh

--- npm-make-registry.sh
+++ npm-make-registry.sh
@@ -1,4 +1,6 @@
 python3 npm-registry.py
+date=$(date '+%Y%m%d%H%M%S')
+"/c/Program Files/7-Zip/7z.exe" a -t7z -mx -m0=LZMA2 -v40m verdaccio-packages-$date.7z packages/ -x!node_modules
 git config user.name github-actions
 git config user.email github-actions@github.com
 git rm -rf packages
