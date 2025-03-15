087e6fe66e84b2bfdc264f146e4fa9ca3ba82349
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-15 12:22:54 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -1,5 +1,5 @@
 rm package.json
 rm package-lock.json
-# cp -f ../package-jsons/package.json package.json
+cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/package-lock.json package-lock.json
-NPM_CONFIG_REGISTRY=http://localhost:4873 npm install create-next-app next
+NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
