7caca08381f2e5f2d94ca6423534dbbf93af737f
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-16 12:43:12 +0900

    Update pnpm-install.sh

--- pnpm-install.sh
+++ pnpm-install.sh
@@ -3,5 +3,4 @@ rm pnpm-lock.yaml
 cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/pnpm-lock.yaml pnpm-lock.yaml
 pnpm config set auto-install-peers true
-NPM_CONFIG_REGISTRY=http://localhost:4873 pnpm install babel-plugin-react-compiler@19.0.0-beta-e552027-20250112 eslint-plugin-import-x
-
+NPM_CONFIG_REGISTRY=http://localhost:4873 pnpm install @npmcli/config
