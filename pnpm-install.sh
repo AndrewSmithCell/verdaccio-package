4bb50c5505f0b03cd141999346e7965bd8acaabc
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-16 10:50:52 +0900

    Update pnpm-install.sh

--- pnpm-install.sh
+++ pnpm-install.sh
@@ -2,5 +2,6 @@ rm package.json
 rm pnpm-lock.yaml
 cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/pnpm-lock.yaml pnpm-lock.yaml
+pnpm config set auto-install-peers true
 NPM_CONFIG_REGISTRY=http://localhost:4873 pnpm install
 
