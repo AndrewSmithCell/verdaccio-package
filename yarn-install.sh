174bb4c24f3e713e343c5fff089517f2cbe25fac
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-25 13:58:21 +0900

    Update yarn-install.sh

--- yarn-install.sh
+++ yarn-install.sh
@@ -2,4 +2,4 @@ rm package.json
 rm yarn.lock
 cp -f ../package-jsons/package.json package.json
 cp -f ../package-jsons/yarn.lock yarn.lock
-NPM_CONFIG_REGISTRY=http://localhost:4873 yarn
+NPM_CONFIG_REGISTRY=http://localhost:4873 yarn --network-concurrency=1
