6500ed7767e29e3c1bc1021037496285321e7b32
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-26 14:38:34 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -2,4 +2,4 @@ rm package.json
 rm package-lock.json
 # cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/package-lock.json package-lock.json
-NPM_CONFIG_REGISTRY=http://localhost:4873 npm install docusaurus express-generator highcharts highcharts-react-official react-virtualized --force --save
+NPM_CONFIG_REGISTRY=http://localhost:4873 npm install docusaurus-theme-search-typesense@next typesense-instantsearch-adapter typesense --force --save
