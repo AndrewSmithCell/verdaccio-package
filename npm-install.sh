3124cc7e8a530c3efee1d52d10f76a19304e6199
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-05-09 08:34:01 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -1,5 +1,6 @@
 rm package.json
 rm package-lock.json
-cp -f ../package-jsons/package.json package.json
+# cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/package-lock.json package-lock.json
-NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
+NPM_CONFIG_REGISTRY=http://localhost:4873 npm install ttf2woff zx react-bits canvas-confetti bulma
+
