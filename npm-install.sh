61be1d6647f9d3290702c3c9211901be53c8eb62
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-05-08 10:43:45 +0900

    Update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -1,5 +1,5 @@
 rm package.json
 rm package-lock.json
-# cp -f ../package-jsons/package.json package.json
+cp -f ../package-jsons/package.json package.json
 # cp -f ../package-jsons/package-lock.json package-lock.json
-NPM_CONFIG_REGISTRY=http://localhost:4873 npm install next-i18next react-i18next i18next --force --save
+NPM_CONFIG_REGISTRY=http://localhost:4873 npm install
